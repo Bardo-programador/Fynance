8e5e71f3b2c69e5f00b035918d3021cd6c97c7cf
Samuel Roberto <samucadeus23@gmail.com>
2023-06-12 22:08:24 -0300

    MACD Function

--- src/fynance/plotting.py
+++ src/fynance/plotting.py
@@ -9,6 +9,9 @@ def candlePlot(df:dict, tempo:int) -> go.Figure:
                                     high=df['High'],
                                     close=df['Close'],
                                     name="Candle")])
+    
+    fig.add_trace(go.Scatter(x=gerarDatas(date.today(), date.today()-timedelta(tempo)), y=df['MediaCurta'], mode='lines', name='MediaMovel'))
+    fig.add_trace(go.Scatter(x=gerarDatas(date.today(), date.today()-timedelta(tempo)), y=df['MediaLonga'], mode='lines', name='MediaMovel'))
     return fig
 
 def gerarDatas(inicio:str, fim:str):
@@ -18,3 +21,35 @@ def gerarDatas(inicio:str, fim:str):
         datas.append(data)
         data -= timedelta(days=1)
     return datas
+
+def gerarMACD(df, tempo):
+        # Criação do gráfico
+    datas = gerarDatas(date.today(), date.today()-timedelta(tempo))
+    df['MACD'] = df['MediaCurta'] - df['MediaLonga']
+    df['Signal'] = df['MACD'].ewm(span=9, adjust=False).mean()
+    fig = go.Figure()
+
+    # Adição das barras positivas
+    fig.add_trace(go.Bar(
+        x=datas,
+        y=df['MACD'],
+        marker=dict(color='green'),
+        opacity=0.7
+    ))
+
+    # Adição das barras negativas
+    fig.add_trace(go.Bar(
+        x=datas,
+        y=df['MACD'].where(df['MACD'] < 0, 0),
+        marker=dict(color='red'),
+        opacity=0.7
+    ))
+
+    # Adição da linha de sinal
+    fig.add_trace(go.Scatter(
+        x=datas,
+        y=df['Signal'],
+        name='Signal',
+        line=dict(color='blue')
+    ))
+    return fig
